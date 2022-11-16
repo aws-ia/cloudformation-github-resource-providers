f0250a2d08055351ff7873b9ea397cfd76d42e76
Eric Beard <ericzbeard@gmail.com>
2022-11-16 13:08:10 -0800

    Fix inputs to make tag unique each time

--- GitHub-Git-Tag/setup.sh
+++ GitHub-Git-Tag/setup.sh
@@ -10,8 +10,8 @@
 
 mkdir -p inputs
 
-cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" | sed "s/GITHUB_SHA/${GITHUB_SHA}/g" > inputs/inputs_1_create.json
-cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" | sed "s/GITHUB_SHA/${GITHUB_SHA}/g" > inputs/inputs_1_update.json
+cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" | sed "s/GITHUB_SHA/${GITHUB_SHA}/g" | sed "s/GITHUB_TAG/$(uuidgen)/g" > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" | sed "s/GITHUB_SHA/${GITHUB_SHA}/g" | sed "s/GITHUB_TAG/$(uuidgen)/g" > inputs/inputs_1_update.json
 cp example-inputs/inputs_1_invalid.json inputs/
 
 python3 ../get_type_configuration.py
