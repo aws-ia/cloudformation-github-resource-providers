1bb40563f2a867401334c22d9c9ac5403c421809
Eric Beard <ericzbeard@gmail.com>
2022-11-21 13:32:39 -0800

    Fix setup for repo to use the same id

--- GitHub-Repositories-Repository/setup.sh
+++ GitHub-Repositories-Repository/setup.sh
@@ -6,12 +6,19 @@
 # values in `example-inputs`.
 #
 #   GITHUB_ORG
+#
+# Example
+#
+#   GITHUB_ORG=ericzbeard-aws-cep-testing AWS_PROFILE=ezbeard-cep ./setup.sh
 
 mkdir -p inputs
 
-cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/$(uuidgen)/g"  > inputs/inputs_1_create.json
-cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/$(uuidgen)/g"  > inputs/inputs_1_update.json
+# Repo name needs to be the same for create and update
+REPO_UUID=$(uuidgen)
+cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${REPO_UUID}/g"  > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${REPO_UUID}/g"  > inputs/inputs_1_update.json
 cp example-inputs/inputs_1_invalid.json inputs/
+
 cat test/integ.yml | sed "s/TEST_REPO_NAME/$(uuidgen)/" > test/integ-unique.yml
 
 python3 ../get_type_configuration.py
