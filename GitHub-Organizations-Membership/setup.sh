129f8a0ff5c34d3d4865158356c134ab1447b90f
Eric Beard <ericzbeard@gmail.com>
2022-11-21 12:08:39 -0800

    Initial commit for org membership

---
+++ GitHub-Organizations-Membership/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script runs in the buildspec before testing
+#
+# It relies on environment variables to be set to replace placeholder
+# values in `example-inputs`.
+#
+#   GITHUB_ORG
+#   GITHUB_REPO
+
+mkdir -p inputs
+
+cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_USERNAME/${GITHUB_USERNAME}/g" > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_USERNAME/${GITHUB_USERNAME}/g" > inputs/inputs_1_update.json
+cp example-inputs/inputs_1_invalid.json inputs/
+
+# Not practical to make anything unique here.. we should delete the stack after
+cat test/integ.yml > test/integ-unique.yml
+
+python3 ../get_type_configuration.py
+
