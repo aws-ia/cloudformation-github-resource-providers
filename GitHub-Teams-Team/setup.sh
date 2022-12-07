0b0d3539564dc8e37471f8053c40fa374c5dbe2b
Eric Beard <ericzbeard@gmail.com>
2022-12-07 14:56:03 -0800

    Add CICD for Teams

---
+++ GitHub-Teams-Team/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This script runs in the buildspec before testing
+#
+# It relies on environment variables to be set to replace placeholder
+# values in `example-inputs`.
+#
+#   GITHUB_ORG
+#
+# Example
+#
+#   GITHUB_ORG=ericzbeard-aws-cep-testing AWS_PROFILE=ezbeard-cep ./setup.sh
+
+mkdir -p inputs
+
+# Repo name needs to be the same for create and update
+REPO_UUID=$(uuidgen)
+cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" > inputs/inputs_1_update.json
+cp example-inputs/inputs_1_invalid.json inputs/
+
+cat test/integ.yml | sed "s/GITHUB_ORG/${GITHUB_ORG}/" > test/integ-unique.yml
+
+python3 ../get_type_configuration.py
+
