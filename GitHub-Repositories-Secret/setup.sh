fe6045cb1c3d8b5e1ffbb029415748cab670af5e
Eric Beard <ericzbeard@gmail.com>
2023-02-03 16:22:08 -0800

    Fix inputs for secrets resources

--- GitHub-Repositories-Secret/setup.sh
+++ GitHub-Repositories-Secret/setup.sh
@@ -5,17 +5,17 @@
 # It relies on environment variables to be set to replace placeholder
 # values in `example-inputs`.
 #
-#   GITHUB_OWNER
+#   GITHUB_ORG
 #   GITHUB_REPO
 #
 #   Example: 
 #
-#    GITHUB_OWNER=ericzbeard GITHUB_REPO=test-stable-1 AWS_PROFILE=ezbeard-cep ./setup.sh
+#    GITHUB_ORG=ericzbeard GITHUB_REPO=test-stable-1 AWS_PROFILE=ezbeard-cep ./setup.sh
 
 mkdir -p inputs
 
-cat example-inputs/inputs_1_create.json | sed "s/GITHUB_OWNER/${GITHUB_OWNER}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" > inputs/inputs_1_create.json
-cat example-inputs/inputs_1_update.json | sed "s/GITHUB_OWNER/${GITHUB_OWNER}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" > inputs/inputs_1_update.json
+cat example-inputs/inputs_1_create.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" > inputs/inputs_1_create.json
+cat example-inputs/inputs_1_update.json | sed "s/GITHUB_ORG/${GITHUB_ORG}/g" | sed "s/GITHUB_REPO/${GITHUB_REPO}/g" > inputs/inputs_1_update.json
 cp example-inputs/inputs_1_invalid.json inputs/
 
 python3 ../get_type_configuration.py
